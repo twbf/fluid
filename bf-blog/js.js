1e2948865577bd9f0a6b0cba7e67ecee72a2626e
twbf <tbuelerf9962@bvsd.org>
2016-02-13 21:59:32 -0900

    javascript success

--- bf-blog/js.js
+++ bf-blog/js.js
@@ -1,32 +1,36 @@
 window.onload = function (evt) {
-    var post = [];
-    var clums = [];
-    for(i = 0; i < 100; i++) {
-        var p = 'p';
-        var id = p.concat(i);;
+    var post = [], i, id;
+    for(i = 0; i < 1000; i++) {
+        id = 'p' + i;
+        if(document.getElementById(id)===false){
+            break;
+        }
         post[i]=document.getElementById(id);
-    }
-    var colls = 5;
-    
-    for(i = 0; i < colls; i++) {
-        clums[i] = 95;
     }
     Array.prototype.min = function() {
       return Math.min.apply(null, this);
     };
-    for(j = 0; j <13; j++) {
-        p = 'p';
-        id = p.concat(j);
-        var clumIndex = clums.indexOf(clums.min());
-        var obj = document.getElementById(id);
-        var collom = clumIndex;
-        obj.style.position = "absolute";
-        obj.style.top = clums[collom] + 'px';
-        obj.style.width = '200px';
-        obj.style.left = collom*225 +10+ 'px';
-        window.alert(collom);
-        post[j]['coll'] = clumIndex;
-        clums[clumIndex] += post[j].offsetHeight + 10;
-        
+    function style(){
+        var clums = [], 
+            colls = Math.floor(window.innerWidth/210);
+        for(i = 0; i < colls; i++) {
+            clums[i] = 100;
+        }
+        var k, obj, collom,
+            wW = window.innerWidth - 10;
+        for(i = 0; i <post.length; i++) {
+            id = 'p' + i;
+            k = clums.indexOf(clums.min());
+            obj = document.getElementById(id).style;
+            obj.position = "absolute";
+            obj.top = clums[k] + 'px';
+            obj.width = (wW-25*colls)/colls + 'px';
+            obj.left = (wW/colls)*k + 10 + 'px';
+            clums[k] += post[i].offsetHeight + 10;
+        }
     }
-}
+    $(window).resize(function(){
+        style();
+    });
+    style();
+};
